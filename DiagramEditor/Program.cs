d12644636f71a2425cfe0a2d73fb073cd991ae14
trporoh <99017248+trporoh@users.noreply.github.com>
2023-04-23 18:52:43 +0300

    Add files via upload

---
+++ DiagramEditor/Program.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using Avalonia.ReactiveUI;
+using System;
+
+namespace DiagramEditor {
+    internal class Program {
+        [STAThread]
+        public static void Main(string[] args) => BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+
+        public static AppBuilder BuildAvaloniaApp()
+            => AppBuilder.Configure<App>()
+                .UsePlatformDetect()
+                .LogToTrace()
+                .UseReactiveUI();
+    }
+}
